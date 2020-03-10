c7cc292830b417ab644981b168cfc0b8aede55fd
Mateusz Piotr Moruś <mpmorus@gmail.com>
2020-03-10 21:24:34 +0100

    Algorytm 1 [6/6]

--- 01 - Podróż do Warszawy/Podróż do Warszawy.cpp
+++ 01 - Podróż do Warszawy/Podróż do Warszawy.cpp
@@ -1,114 +1,130 @@
 #include <algorithm>
 #include <iostream>
-#include <map>
-#include <queue>
 #include <vector>
+#include <queue>
 
 typedef unsigned short ushort;
 typedef unsigned int uint;
 
-// Struktura reprezentuj¹ca punkt postoju
+struct Stop;
+
+// Struktura reprezentuj¹ca krawêdŸ miêdzy postojami
+struct Edge {
+    Stop* const stop_a; // WskaŸnik na postój
+    Stop* const stop_b; // WskaŸnik na postój
+    const uint weight;  // Waga krawêdzi
+
+    // Konstruktor krawêdzi inicjalizuj¹cy zmienne
+    Edge(Stop* a, Stop* b, uint w) : stop_a(a), stop_b(b), weight(w) {}
+};
+
+// Struktura reprezentuj¹ca postój
 struct Stop {
-    ushort number;      // Numer postoju
-    uint shortest_path; // Najkrótsza odleg³oœæ od postoju pocz¹tkowego
-    uint x, y;          // Koordynaty postoju
-    std::map<Stop*, ushort> connections;
+    const ushort number; // Numer postoju
+    const uint x, y; // Koordynaty postoju
+    std::vector<Edge*> edges; // Wektor krawêdzi tego wierzcho³ka - postoju
+    uint shortest_path;
 
-    // Konstruktor inicjalizuj¹cy najkrótsza trasê do wielkoœci maksymalnej
-    Stop(ushort number) : shortest_path(-1), number(number) {}
+    // Konstruktor przyjmuj¹cy numer i koordynaty, i inicjalizuj¹cy zmienne
+    Stop(ushort number, uint x, uint y) : number(number), x(x), y(y), shortest_path(-1) {}
 
-    // Funkcja tworz¹ca krawêdŸ miêdzy tym przystankiem a podanym w argumencie
-    void connect_with(Stop*& other) {
-        uint weight = std::min(abs((int)x - (int)other->x), abs((int)y - (int)other->y));
-        connections[other] = weight;
-        other->connections[this] = weight;
+    // Funkcja zwracaj¹ca odleg³oœæ do postoju podanego w argumencie
+    uint distance_to(const Stop& other) const {
+        return std::min(abs((int)this->x - (int)other.x), abs((int)this->y - (int)other.y));
     }
 
-    uint get_weight_to(Stop*& other) {
-        if (other == this) return 0;
-        auto pos = connections.find(other);
-        if (pos == connections.end()) return -1;
-        return pos->second;
+    // Funkcja tworz¹ca krawêdŸ do podanego postoju i zwracaj¹ca na ni¹ wskaŸnik
+    Edge* connect_to(Stop& other) {
+        Edge* edge = new Edge(this, &other, distance_to(other));
+        this->edges.push_back(edge);
+        other.edges.push_back(edge);
+        return edge;
     }
 };
 
-// Klasa reprezentuj¹ca trasê przejazdu
+// Klasa reprezentuj¹ca trasê Partycji
 class Route {
-    std::vector<Stop*> stops; // Tablica wskaŸników na postoje
-    Stop* start;               // WskaŸnik na postój pocz¹tkowy - punkt startowy
-    Stop* end;                 // WskaŸnik na postój koñcowy - punkt docelowy
+    std::vector<Stop> stops; // Wektor postojów
+    std::vector<Edge*> edges; // Wektor wskaŸników na wszystkie krawêdzie
+    Stop* start; // WskaŸnik na postój startowy
+    Stop* destination; // WskaŸnik na postój docelowy
 
 public:
-    // Konstruktor przyjmuj¹cy liczbê postojów, wczytuj¹cy potrzebne dane i inicjalizuj¹cy zmienne
-    Route(ushort nof_stops) : stops(nof_stops) {
-        for (ushort i = 0; i < nof_stops; i++) {
-            stops[i] = new Stop(i);
-            std::cin >> stops[i]->x >> stops[i]->y;
+
+    // Konstrutkor przyjmuj¹cy liczbê postojów, wczytuj¹cy dane oraz generuj¹cy na ich podstawie graf
+    Route(ushort nof_stops) : start(nullptr), destination(nullptr) {
+        std::vector<std::pair<ushort, uint>> x_neighbours;
+        std::vector<std::pair<ushort, uint>> y_neighbours;
+        for(ushort i = 0; i < nof_stops; i++) {
+            uint x, y;
+            std::cin >> x >> y;
+            stops.emplace_back(i, x, y);
+            x_neighbours.emplace_back(i, x);
+            y_neighbours.emplace_back(i, y);
         }
-        ushort start_number, end_number;
-        std::cin >> start_number >> end_number;
-        start = stops[start_number];
-        end = stops[end_number];
-        std::sort(stops.begin(), stops.end(), [](Stop*& left, Stop*& right) {
-            return left->x < right->x;
-            });
-        for (ushort i = 1; i < nof_stops - 1; i++) {
-            stops[i - 1]->connect_with(stops[i]);
-            stops[i]->connect_with(stops[i + 1]);
-        }
-        std::sort(stops.begin(), stops.end(), [](Stop*& left, Stop*& right) {
-            return left->y < right->y;
-            });
-        for (ushort i = 1; i < nof_stops - 1; i++) {
-            stops[i - 1]->connect_with(stops[i]);
-            stops[i]->connect_with(stops[i + 1]);
+        ushort s, d;
+        std::cin >> s >> d;
+        start = &stops[s];
+        destination = &stops[d];
+        std::sort(x_neighbours.begin(), x_neighbours.end(),
+                  [](auto& a, auto& b) {
+                      return a.second < b.second;
+                  });
+        std::sort(y_neighbours.begin(), y_neighbours.end(),
+                  [](auto& a, auto& b) {
+                      return a.second < b.second;
+                  });
+        for(ushort i = 1; i < nof_stops; i++) {
+            ushort prev = x_neighbours[i - 1].first;
+            ushort curr = x_neighbours[i].first;
+            edges.push_back(stops[prev].connect_to(stops[curr]));
+            prev = y_neighbours[i - 1].first;
+            curr = y_neighbours[i].first;
+            edges.push_back(stops[prev].connect_to(stops[curr]));
         }
     }
 
-    // Destruktor usuwaj¹cy zmienne stworzone dynamicznie
+    // Destruktor usuwaj¹cy dynamicznie stworzone krawêdzie
     ~Route() {
-        for (auto& stop : stops) {
-            delete stop;
+        for(auto& edge : edges) {
+            delete edge;
         }
     }
 
-    // Funkcja zwracajaca d³ugoœæ najkrótszej drogi z postoju startowego do docelowego
-    uint shortest_path() {
-        auto cmp = [](Stop* left, Stop* right) {
-            return left->shortest_path > right->shortest_path;
-        };
-        std::priority_queue<Stop*, std::vector<Stop*>, decltype(cmp)> queue(cmp);
-        for (ushort i = 0; i < stops.size(); i++) {
-            stops[i]->shortest_path = start->get_weight_to(stops[i]);
-            if (stops[i] != start) queue.push(stops[i]);
-        }
-        Stop* current_stop = queue.top();
-        while (current_stop != end) {
-            if (current_stop->shortest_path < end->shortest_path) {
-                for (auto& stop : stops) {
-                    if (stop == start || stop == current_stop) continue;
-                    uint current_stop_to_stop = current_stop->get_weight_to(stop);
-                    if (current_stop_to_stop == (unsigned)-1) continue;
-                    uint new_path = current_stop_to_stop + current_stop->shortest_path;
-                    if (new_path < stop->shortest_path) {
-                        stop->shortest_path = new_path;
-                        queue.push(stop);
+    // Funkcja zwracaj¹ca nakrótsz¹ odleg³oœæ od postoju pocz¹tkowego do docelowego
+    uint shortest_path() const {
+        std::priority_queue<std::pair<uint, Stop*>, std::vector<std::pair<uint, Stop*>>, std::greater<std::pair<uint, Stop*>>> queue;
+        start->shortest_path = 0;
+        Stop* pos = start;
+        while(pos != destination) {
+            for(auto& edge : pos->edges) {
+                Stop* const other = ((edge->stop_a == pos) ? edge->stop_b : edge->stop_a);
+                if(other->shortest_path == (uint)-1) {
+                    other->shortest_path = pos->shortest_path + edge->weight;
+                    queue.emplace(other->shortest_path, other);
+                } else {
+                    uint new_path = pos->shortest_path + edge->weight;
+                    if(new_path < other->shortest_path) {
+                        other->shortest_path = new_path;
+                        queue.emplace(other->shortest_path, other);
                     }
                 }
             }
+            pos = queue.top().second;
             queue.pop();
-            current_stop = queue.top();
         }
-        return end->shortest_path;
+        return destination->shortest_path;
     }
+
 };
 
 int main() {
+
     ushort n; // Liczba postojów
 
     std::cin >> n;
 
-    Route route(n); // Obiekt trasy przejazdu
+    Route route(n);
 
     std::cout << route.shortest_path() << std::endl;
 
